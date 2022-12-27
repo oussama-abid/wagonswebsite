cca424db7c2113e8cc2ba21c1f6a34d7f828515b
oussama-abid <oussamaabid52@gmail.com>
2022-12-27 22:39:56 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 
 Route::get('/test', function () {
-    return view('test');
-=======
+    return view('test');});
+
 Route::get('/home', function () {
     return view('home');
->>>>>>> 5ac39061f5cdf40500a8ee90e50f2a76832f9972
 });
