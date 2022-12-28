a3ad09874411ea7b1f265120863db99d2e010c07
oussama-abid <oussamaabid52@gmail.com>
2022-12-28 20:55:32 +0100

    migration,model,controller

---
+++ database/migrations/2022_12_28_194406_create_zugs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateZugsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('zugs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('nachname');
+            $table->string('versandbanhof');
+            $table->string('bestimmungsbanhof');
+            $table->date('datum');
+            $table->integer('zugnummer');
+            $table->string('Mindestbremshunderstel');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('zugs');
+    }
+}
